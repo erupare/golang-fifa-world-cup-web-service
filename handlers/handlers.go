155fe3f8c327e57c661c1be454982e18c7bdbdd8
Edgar Rupare <erupar200@caledonian.ac.uk>
2022-02-09 18:41:46 +0000

    Respond with JSON data

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
@@ -11,7 +12,23 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
-
+	res.Header().Set("Content-Type", "application/json")
+	year := req.URL.Query().Get("year")
+	if year == "" {
+		winners, err := data.ListAllJSON()
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Write(winners)
+	} else {
+		filteredWinners, err := data.ListAllByYear(year)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		res.Write(filteredWinners)
+	}
 }
 
 // AddNewWinner adds new winner to the list
